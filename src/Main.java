e76dff1b66624280bee33726d6612c2696743618
sermen8878 <sermen88@yandex.ru>
2025-03-06 23:31:23 +0500

    Task-4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        // Инициализация переменной year
-        var year = 2021;  // Год для проверки
-
-        // Проверка, является ли год високосным
-        if (year <= 1584) {
-            System.out.println(year + " год не поддерживается (должен быть больше 1584)");
-        } else if ((year % 400 == 0) || (year % 100 != 0 && year % 4 == 0)) {
-            System.out.println(year + " год является високосным");
+        var deliveryDistance = 95;
+        var deliveryTime = 0;
+        if (deliveryDistance <= 20) {
+            deliveryTime = 1;
+        } else if (deliveryDistance <= 60) {
+            deliveryTime = 2;
+        } else if (deliveryDistance <= 100) {
+            deliveryTime = 3;
         } else {
-            System.out.println(year + " год не является високосным");
+            System.out.println("Доставка свыше 100 км невозможна");
+            return;
         }
+
+        System.out.println("Потребуется дней: " + deliveryTime);
     }
 }
